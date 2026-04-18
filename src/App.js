87ff8dd6c87c73d89302f0573b80c62e5f588357
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-04-18 12:22:54 +0530

    new project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+
+
+function App() {
+  return (
+
+    <>
+
+    <Header />
+    <Home />
+    <Footer />
+
+    </>
+
+     // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './Home.css';
+
+ 
+function Home(){
+    return(
+        <p class="text">This is content area.</p>
+    );
+}
+
+export default Home;
