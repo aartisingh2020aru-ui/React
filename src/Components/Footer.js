29b521a08ff306159407805c40e02a2313258fa5
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-04-18 23:25:39 +0530

    somthing

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer(){
     return(
-        <h1>This is Footer!  making trubble</h1>
+        <>
+        <h1>This is Footer!  making trubble.</h1>
+        <h2>hello error</h2>
+        </>
         
     );
 }
